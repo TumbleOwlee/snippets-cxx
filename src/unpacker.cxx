092e038b4fbb92e16320d2493a09ce0c1ea152a9
David Loewe <49597367+TumbleOwlee@users.noreply.github.com>
2023-08-21 01:01:18 +0200

    Update unpacker.cxx

--- src/unpacker.cxx
+++ src/unpacker.cxx
@@ -5,40 +5,46 @@
 template<typename... Types>
 struct Tuple {
     typedef std::tuple<Types...> Type;
+    typedef std::tuple_size<Type> Size;
 };
 
 template<size_t len, typename C, typename T, typename... Args>
 struct Unpacker {
-    static inline void call(std::function<C> cb, typename T::Type& t, Args... args) {
-        Unpacker<len-1, C, T, std::tuple_element_t<len-1, typename T::Type>, Args...>::call(cb, t, std::get<len-1>(t), args...);
+    static inline void call(std::function<C> cb, typename T::Type& t, Args&... args) {
+        typedef typename std::tuple_element<len-1, typename T::Type>::type Tail;
+        Unpacker<len-1, C, T, Tail, Args...>::call(cb, t, std::get<len-1>(t),  args...);
     }
 };
 
 template<typename C, typename T, typename... Args>
 struct Unpacker<0, C, T, Args...> {
-    typedef typename T::Type TupleType;
-    static inline void call(std::function<C> cb, TupleType& t, Args... args) {
+    static inline void call(std::function<C> cb, typename T::Type& t, Args&... args) {
         cb(args...);
     }
 };
 
-template<typename... Args>
-static inline void call(std::function<void(Args...)> cb, std::tuple<Args...> t) {
-    Unpacker<std::tuple_size_v<std::tuple<Args...>>, void(Args...), Tuple<Args...>>::call(cb, t);
+template<typename F, typename... Args>
+static inline void call(F cb, std::tuple<Args...> t) {
+    std::function<void(Args...)> fn = cb;
+    Unpacker<typename Tuple<Args...>::Size{}, void(Args...), Tuple<Args...>>::call(fn, t);
 }
 
 int main(int, char**) {
     {    
         std::tuple<> t;
-        std::function<void()> fn = []() { std::cout << "No parameters" << std::endl; };
-        call(fn, t);
+        call([]() { std::cout << "No parameters" << std::endl; }, t);
     }
     {    
-        std::tuple<int, bool, float> t = {10, true, 33.33};
-        std::function<void(int, bool, float)> fn = [](int v1, bool v2, float v3) {
-            std::cout << "Parameters: " << v1 << ", " << v2 << ", " << v3 << std::endl; 
-        };
-        call(fn, t);
+        std::tuple<int> t = {10};
+        call([](int v1) { std::cout << v1 << std::endl; }, t);
+    }
+    {    
+        std::tuple<int, bool> t = {0, true};
+        call([](int v1, bool v2) { std::cout << v1 << ", " << v2 << std::endl; }, t);
+    }
+    {    
+        std::tuple<int, bool, float> t = {0, true, 33.33};
+        call([](int v1, bool v2, float v3) { std::cout << v1 << ", " << v2 << ", " << v3 << std::endl; }, t);
     }
     return 0;
 }
